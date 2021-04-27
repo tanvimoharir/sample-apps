f7dbe2fb1714bf5adef27edfb2f9b366144b8f8f
tanvimoharir <tanvimoharir@gmail.com>
2021-04-27 18:26:16 +0530

    DI stuff

--- BooksApi/Startup.cs
+++ BooksApi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using BooksApi.Models;
 
 namespace BooksApi
 {
@@ -29,7 +30,17 @@ namespace BooksApi
         {
             // Add framework services.
             services.Configure<BookstoreDatabasesettings>(Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
+            //The configuration instance to which appsettings.json file's BookstoreDatabaseSettings section binds is registered in DI container
+            //DI container is a framework for implementing automatic Dependency injection
+            //The 'container' creates an object of the specified class and also injects all the dependency objects through a constructor,
+            //a property or a method at run time and disposes it at the appropriate time so that we dont have to create/manage objects manually
+            //For example BookstoreDatabaseSettings' object's ConnectionString property is populated with the BookstoreDatabaseSettings:ConnectionString
+            //property in appsettings.json
             services.AddSingleton<IBookstoreDatabaseSettings>(sp => sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>.Value());
+            //The IBookstoreDatabaseSettings interface is registered in DI with a singleton service.
+            //When injected,the interface resolves to a BookstoreDatabaseSettings object
+            //By 'registering' in DI container - the container must know which depedency to instantiate,this process is called registration
+            //While resolving depedency,the container will create the objects automatically(this process is called resolve)
             services.AddControllers();
         }
 
